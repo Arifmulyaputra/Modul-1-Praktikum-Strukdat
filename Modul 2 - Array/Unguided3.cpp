b876c0fba841ee0064a07f0d7203521f5143e8e4
Arif Mulya Putra <arif.mly15ptr@gmail.com>
2024-03-24 17:07:28 +0700

    done, kali

---
+++ Modul 2 - Array/Unguided3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukkan jumlah elemen array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Masukkan " << n << " angka:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    // Menghitung nilai minimum
+    int min = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+    }
+    double average = static_cast<double>(sum) / n;
+
+    cout << "Nilai Maksimum: " << max << endl;
+    cout << "Nilai Minimum: " << min << endl;
+    cout << "Nilai Rata-rata: " << average << endl;
+
+    return 0;
+}
